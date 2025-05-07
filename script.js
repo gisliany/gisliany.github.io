1056f4197be99377812c8bfdf334e6f73926a8c7
Gisliany <gisliany.alves@gmail.com>
2025-05-07 18:26:36 -0300

    temp files

---
+++ script.js
@@ -0,0 +1,156 @@
+
+(function(){
+	
+	var Memory = {
+
+		init: function(cards){
+			this.$game = $(".game");
+			this.$modal = $(".modal");
+			this.$overlay = $(".modal-overlay");
+			this.$restartButton = $("button.restart");
+			this.cardsArray = $.merge(cards, cards);
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+		},
+
+		shuffleCards: function(cardsArray){
+			this.$cards = $(this.shuffle(this.cardsArray));
+		},
+
+		setup: function(){
+			this.html = this.buildHTML();
+			this.$game.html(this.html);
+			this.$memoryCards = $(".card");
+			this.paused = false;
+     	this.guess = null;
+			this.binding();
+		},
+
+		binding: function(){
+			this.$memoryCards.on("click", this.cardClicked);
+			this.$restartButton.on("click", $.proxy(this.reset, this));
+		},
+		// kinda messy but hey
+		cardClicked: function(){
+			var _ = Memory;
+			var $card = $(this);
+			if(!_.paused && !$card.find(".inside").hasClass("matched") && !$card.find(".inside").hasClass("picked")){
+				$card.find(".inside").addClass("picked");
+				if(!_.guess){
+					_.guess = $(this).attr("data-id");
+				} else if(_.guess == $(this).attr("data-id") && !$(this).hasClass("picked")){
+					$(".picked").addClass("matched");
+					_.guess = null;
+				} else {
+					_.guess = null;
+					_.paused = true;
+					setTimeout(function(){
+						$(".picked").removeClass("picked");
+						Memory.paused = false;
+					}, 600);
+				}
+				if($(".matched").length == $(".card").length){
+					_.win();
+				}
+			}
+		},
+
+		win: function(){
+			this.paused = true;
+			setTimeout(function(){
+				Memory.showModal();
+				Memory.$game.fadeOut();
+			}, 1000);
+		},
+
+		showModal: function(){
+			this.$overlay.show();
+			this.$modal.fadeIn("slow");
+		},
+
+		hideModal: function(){
+			this.$overlay.hide();
+			this.$modal.hide();
+		},
+
+		reset: function(){
+			this.hideModal();
+			this.shuffleCards(this.cardsArray);
+			this.setup();
+			this.$game.show("slow");
+		},
+
+		
+		shuffle: function(array){
+			var counter = array.length, temp, index;
+	   	while (counter > 0) {
+        	index = Math.floor(Math.random() * counter);
+        	counter--;
+
+        	temp = array[counter];
+        	array[counter] = array[index];
+        	array[index] = temp;
+	    	}
+	    	return array;
+		},
+
+		buildHTML: function(){
+			var frag = '';
+			this.$cards.each(function(k, v){
+				frag += '<div class="card" data-id="'+ v.id +'"><div class="inside">\
+				<div class="front"><img src="'+ v.img +'"\
+				alt="'+ v.name +'" /></div>\
+				<div class="back"><img src="img/happy_bday.jpg"\
+				alt="Codepen" /></div></div>\
+				</div>';
+			});
+			return frag;
+		}
+	};
+
+	var cards = [
+		{
+			name: "familia",
+			img: "img/familia.jpg",
+			id: 1,
+		},
+		{
+			name: "geninho_psicogato",
+			img: "img/geninho_psicogato.png",
+			id: 2
+		},
+		{
+			name: "amy_surtada",
+			img: "img/amy_surtada.png",
+			id: 3
+		},
+		{
+			name: "casal",
+			img: "img/casal.png",
+			id: 4
+		}, 
+		{
+			name: "praia",
+			img: "img/praia.png",
+			id: 5
+		},
+		{
+			name: "tassia",
+			img: "img/tassia.png",
+			id: 6
+		},
+		{
+			name: "vinicius",
+			img: "img/vinicius.png",
+			id: 7
+		},
+		{
+			name: "pintim_amarelim",
+			img: "img/pintim_amarelim.png",
+			id: 8
+		}
+	];
+    
+	Memory.init(cards);
+
+})();
